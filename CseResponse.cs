c78c20d5a880e9491d4b207df19a515d29f46af1
Lei Sun <leis@microsoft.com>
2017-07-31 15:20:54 -0700

    first commit

---
+++ CseResponse.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CseResponse.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the CseResponse type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace CsePlayer
+{
+    using Newtonsoft.Json;
+
+    public class CseResponse
+    {
+        [JsonProperty(PropertyName = "items")]
+        public List<CseSearchResult> CseSearchResults { get; set; }
+    }
+
+    public class CseSearchResult
+    {
+        [JsonProperty(PropertyName = "kind")]
+        public string ResultKind { get; set; }
+
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "htmlTitle")]
+        public string HtmlTitle { get; set; }
+
+        [JsonProperty(PropertyName = "link")]
+        public string Link { get; set; }
+
+        [JsonProperty(PropertyName = "displayLink")]
+        public string DisplayLink { get; set; }
+
+        [JsonProperty(PropertyName = "snippet")]
+        public string Snippet { get; set; }
+
+        [JsonProperty(PropertyName = "htmlSnippet")]
+        public string HtmlSnippet { get; set; }
+    }
+}
---
+++ Scrape.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Scrape.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Scrape type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CsePlayer
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    public class Scrape
+    {
+        public string SubmitTime { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Description { get; set; }
+
+        public string Engine { get; set; }
+
+        [XmlElement(ElementName = "EntitySetId")]
+        public string QuerySetGuid { get; set; }
+
+        [XmlElement(ElementName = "MiniSet")]
+        public string QuerySetName { get; set; }
+
+        [XmlElement(ElementName = "QueryCount")]
+        public string SerpCount { get; set; }
+
+        public string Market { get; set; }
+
+        public string ScrapeDepth { get; set; }
+
+        [XmlArray("Data")]
+        [XmlArrayItem("Query")]
+        public List<Serp> Serps { get; set; }
+    }
+
+    public class Serp
+    {
+        [XmlAttribute("id")]
+        public string QueryGuid { get; set; }
+
+        public string RawText { get; set; }
+
+        public string Text { get; set; }
+
+        public string Market { get; set; }
+
+        [XmlArray("Results")]
+        [XmlArrayItem("Result")]
+        public List<Result> Results { get; set; }
+    }
+
+    public class Result
+    {
+        [XmlAttribute("AdjustedRank")]
+        public string AdjustedRank { get; set; }
+
+        public int Position { get; set; }
+
+        public string Title { get; set; }
+
+        [XmlElement("URL")]
+        public string Url { get; set; }
+
+        [XmlElement("DisplayURL")]
+        public string DisplayUrl { get; set; }
+
+        public string Snippet { get; set; }
+
+        public string Language { get; set; }
+
+        public string Tier { get; set; }
+    }
+
+    public class ScrapeExtension
+    {
+        public static Scrape GenerateScrapeFromCseResponse(CseResponse cseResponse)
+        {
+            return null;
+        }
+    }
+}
